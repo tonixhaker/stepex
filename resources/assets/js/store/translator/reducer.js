f612c296288eba2d564bb13a950dbc11c4a26158
tonixhaker <tonixhaker@gmail.com>
2018-06-12 21:20:10 +0300

    Langs list

--- resources/assets/js/store/translator/reducer.js
+++ resources/assets/js/store/translator/reducer.js
@@ -46,7 +46,7 @@ export default (state = initialState, action) => {
             return processReducer(state);
         }
         case success(types.LANGS) : {
-            return {...state, status: STATE_STATUSES.READY, langs:'TEST'}
+            return {...state, status: STATE_STATUSES.READY, ...action.payload.data}
         }
         case error(types.LANGS) : {
             return errorReducer(action.payload.response.data);
